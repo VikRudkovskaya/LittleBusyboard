a28f46cbbb5e708d072979b5d4d5cebfab6e67a7
VikRudkovskaya <vik-rudkovskaya@yandex.ru>
2019-01-26 11:24:31 +0300

    Add. First level

---
+++ LittleBusyBoard/Models/Background.swift
@@ -0,0 +1,23 @@
+//
+//  Background.swift
+//  LittleBusyboard
+//
+//  Created by Viktoria Rudkovskaya on 26/01/2019.
+//  Copyright © 2019 VR/RN. All rights reserved.
+//
+
+import UIKit
+
+class Background: NSObject {
+    
+    var identifire: String?
+    
+    var name: String?
+    
+    var bgDescription: String?
+    
+    var fileName: String?
+    
+    var url: String?
+    
+}
