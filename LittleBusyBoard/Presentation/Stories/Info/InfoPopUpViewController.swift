26796b1ad5646eb64e7e9419ea2a198faef670fe
VikRudkovskaya <vik-rudkovskaya@yandex.ru>
2019-02-08 12:44:23 +0300

    Add. Dismiss info pop up

--- LittleBusyBoard/Presentation/Stories/Info/InfoPopUpViewController.swift
+++ LittleBusyBoard/Presentation/Stories/Info/InfoPopUpViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class InfoPopUpViewController: UIViewController {
+    @IBOutlet var scrollView: UIScrollView!
+    
+    convenience init() {
+        self.init(nibName: nil, bundle: nil)
+        
+        modalTransitionStyle = .crossDissolve
+        modalPresentationStyle = .overCurrentContext
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.scrollView.delegate = self
+
     }
     
     @IBAction func closeTouchUpInside(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+
+}
+
+extension InfoPopUpViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.contentOffset.y < -150 {
+            self.dismiss(animated: true, completion: nil)
+        }
     }
 }
